fe7abe0eff79d00fad50dcc134528cfb641d8b86
Luis Mendoza <luis.mendoza.alonso@hotmail.com>
2024-04-05 18:05:29 -0500

    Se crea clase CustomError para centralizar y personalizar los errores.

--- src/application/services/auth/auth.service.ts
+++ src/application/services/auth/auth.service.ts
@@ -4,6 +4,13 @@ import { AuthRepository, CustomError, UserDtoValidator } from "../../../domain";
 export class AuthService {
     constructor(private readonly authRepository: AuthRepository) { }
 
+
+    // Validación de la existencia del correo electrónico
+  public async isEmailAvailable(email: string): Promise<boolean> {
+    const existingUser = await this.authRepository.findUserByEmail(email);
+    return !existingUser; 
+  }
+
     public async registerUser(userDto: UserDtoValidator): Promise<void> {
         const existingUser = await this.authRepository.findUserByEmail(userDto.email);
         if (existingUser) throw CustomError.badRequest("User already registered");
@@ -17,6 +24,7 @@ export class AuthService {
             throw CustomError.internalServer("Failed to register user");
         }
     }
+    
 }
 
 
--- src/domain/errors/custom-error.ts
+++ src/domain/errors/custom-error.ts
@@ -1,31 +1,41 @@
 export class CustomError extends Error {
-
     constructor(
-        public readonly statusCode: number,
-        public readonly message: string,
+      public readonly statusCode: number,
+      public readonly message: string,
+      public readonly timestamp: Date = new Date(),
+      public readonly stackTrace?: string,
     ) {
-        super(message);
+      super(message);
     }
-
+  
     static badRequest(message: string) {
-        return new CustomError(400, message);
+      return new CustomError(400, message);
     }
-
+  
     static unauthorized(message: string) {
-        return new CustomError(401, message);
+      return new CustomError(401, message);
     }
-
+  
     static forbidden(message: string) {
-        return new CustomError(403, message);
+      return new CustomError(403, message);
     }
-
+  
     static notFound(message: string) {
-        return new CustomError(404, message);
+      return new CustomError(404, message);
     }
-
+  
     static internalServer(message: string) {
-        return new CustomError(500, message);
+      return new CustomError(500, message);
     }
-
-
-}
+  
+    toJSON() {
+      return {
+        statusCode: this.statusCode,
+        message: this.message,
+        timestamp: this.timestamp.toISOString(),
+        stackTrace: this.stackTrace,
+      };
+    }
+  }
+  
+  
